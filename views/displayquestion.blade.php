b221f6c8c8fa148034372a9de170c7e6d40a2623
Detti <bernadett1998@gmail.com>
2025-09-23 13:56:30 +0200

    try tailwind style

--- views/displayquestion.blade.php
+++ views/displayquestion.blade.php
@@ -5,8 +5,8 @@
 
 @section('content')
     @if($question)
-        <x-header>Question</x-header>
-        <div>
+        <div class="mx-auto max-w-3xl p-4 space-y-3">
+        <x-header>Question ID: {{$question->id}}</x-header>
     <h3>Title: {{ $question->title }}</h3>
     <p>Details: {{ $question->message }}</p>
         <h3>Answers: </h3>
@@ -34,7 +34,7 @@
             <p>No answers yet.</p>
             @endif
         @if(isset($_SESSION['user_id']))
-        <a href="/add-answer">Add answer</a>
+        <a href="/add-answer" class="inline-block text-white hover:underline focus:ring-1 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Add answer</a>
         @else
             <p><em>You must be logged in to add an answer.</em></p>
         @endif
