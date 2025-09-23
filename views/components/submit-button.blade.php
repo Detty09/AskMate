80137b010d175e68d519a5acb3283f823091cb59
BrindzaB <63659791+BrindzaB@users.noreply.github.com>
2025-09-23 19:38:18 +0200

    tailwind styling added

--- views/components/submit-button.blade.php
+++ views/components/submit-button.blade.php
@@ -1,5 +1,5 @@
 <button type="submit"
-        class="text-white bg-blue-700 hover:bg-blue-500 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none focus:ring-blue-800">
+        class="text-white bg-blue-700 hover:bg-blue-500 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 transition-transform duration-500 ease-in-out transform hover:scale-105">
     {{ $slot }}
 </button>
 
