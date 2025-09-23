2e91992db8b895b27187d010e5572dd1c4328610
liaak <190502806+liaak@users.noreply.github.com>
2025-09-23 14:11:10 +0200

    i need newest push

--- src/Repository/QuestionRepository.php
+++ src/Repository/QuestionRepository.php
@@ -77,4 +77,9 @@ class QuestionRepository implements RepositoryInterface {
         $stmt = $this->pdo->prepare("DELETE FROM question WHERE id = :id");
         $stmt->execute([':id' => $id]);
     }
+
+    public function increaseVote(int $id, int $increment): void {
+        $stmt = $this->pdo->prepare("UPDATE question SET vote_number = vote_number + :increment WHERE id = :id");
+        $stmt->execute(['increment' => $increment, 'id' => $id]);
+    }
 }
