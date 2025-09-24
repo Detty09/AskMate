5f3bbb527806077888a1667dab4d020a42f43cb1
BrindzaB <63659791+BrindzaB@users.noreply.github.com>
2025-09-24 14:05:44 +0200

    question and answers view design implemented

--- views/answer_form.blade.php
+++ views/answer_form.blade.php
@@ -3,10 +3,14 @@
 @section("title", "Add new answer")
 
 @section('content')
-    <h1>Add new answer:</h1>
-    <form action="/submit-answer" method="POST">
-        <input type="hidden" name="id_question" value="{{ $id_question }}">
-        <textarea name="answer-message" required></textarea>
-        <x-submit-button type="submit">Submit</x-submit-button>
-    </form>
+    <div class="flex flex-col items-center mt-6 min-w-1/3">
+        <x-header>Add new answer:</x-header>
+        <div class="flex flex-col w-full max-w-3xl mt-6">
+            <form action="/submit-answer" method="POST" class="flex flex-col gap-6">
+                <input type="hidden" name="id_question" value="{{ $id_question }}">
+                <textarea name="answer-message" placeholder="Type something..." required class="p-2 min-h-50 text-black border border-gray-200 rounded-xl"></textarea>
+                <x-submit-button type="submit">Submit</x-submit-button>
+            </form>
+        </div>
+    </div>
 @endsection
--- views/displayquestion.blade.php
+++ views/displayquestion.blade.php
@@ -5,48 +5,70 @@
 
 @section('content')
     @if($question)
-        <div class="mx-auto max-w-3xl p-4 space-y-3">
-        <x-header>Question ID: {{$question->id}}</x-header>
-    <h3>Title: {{ $question->title }}</h3>
-    <p>Details: {{ $question->message }}</p>
-            @if($image)
-                <div class="my-4">
-                    <img src="{{ $image->directory . $image->file_name }}"
-                         alt="Question image"
-                         class="w-32 h-auto rounded shadow-md">
-                </div>
-            @endif
-        <h3>Answers: </h3>
-        @if (count($answers) > 0)
-        <ul>
-            @foreach($answers as $answer)
-                <div>
-                <li>{{$answer->message}}</li>
-                @if(isset($_SESSION['user_id']) && $answer->id_registered_user == $_SESSION['user_id'])
-                        <form action="/answer-edit" method="GET" style="display:inline;">
-                            <input type="hidden" name="id" value="{{ $answer->id }}">
-                            <x-submit-button type="submit">Edit</x-submit-button>
-                        </form>
-                    <form action="/answer-delete" method="POST" style="display:inline;">
-                        <input type="hidden" name="id" value="{{ $answer->id }}">
-                        <x-submit-button type="submit" onclick="return confirm('Are you sure you want to delete this answer?')">
-                            Delete
-                        </x-submit-button>
-                    </form>
+        <div class="flex flex-col w-full items-center p-4 gap-10">
+            <div class="flex p-4 gap-10 items-center">
+                @if($image)
+                    <div class="my-4">
+                        <img src="{{ $image->directory . $image->file_name }}"
+                             alt="Question image"
+                             class="w-32 h-auto rounded shadow-md">
+                    </div>
                 @endif
+                <div>
+                    <h3 class="text-5xl font-medium text-gray-900 text-center mb-5">{{ $question->title }}</h3>
+                    <p class="text-xl font-normal text-gray-900 text-center">{{ $question->message }}</p>
                 </div>
-            @endforeach
-        </ul>
-        @else
-            <p>No answers yet.</p>
-            @endif
-        @if(isset($_SESSION['user_id']))
-        <a href="/add-answer" class="inline-block text-black hover:underline focus:ring-1 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Add answer</a>
-        @else
-            <p><em>You must be logged in to add an answer.</em></p>
-        @endif
-    @else
-        <h1>Question not found</h1>
-    @endif
+            </div>
+            <div class="flex flex-col p-4 w-full gap-10 items-center">
+                <div class="w-1/2">
+                    <h5 class="text-xl font-medium text-gray-900 text-center mb-4">Answers: </h5>
+                    @if (count($answers) > 0)
+                            @foreach($answers as $answer)
+                                <div class="w-full">
+                                    <div class="border border-gray-200 rounded-xl shadow-sm p-6 mb-4">
+                                        <div class="flex justify-between items-center">
+                                            <div class="flex flex-col">
+                                                <h3 class="text-xl font-medium text-black">{{$answer->message}}</h3>
+                                            </div>
+                                            @if(isset($_SESSION['user_id']) && $answer->id_registered_user == $_SESSION['user_id'])
+                                                <div>
+                                                    <form action="/answer-edit" method="GET" style="display:inline;">
+                                                        <input type="hidden" name="id" value="{{ $answer->id }}">
+                                                        <button type="submit"
+                                                                class="px-3 py-1 text-sm text-white bg-yellow-500 rounded-lg hover:bg-yellow-300 transition-colors transition-transform duration-100 ease-in-out transform hover:scale-105">
+                                                            <i class="fa-solid fa-pen"></i>
+                                                        </button>
+                                                    </form>
+                                                    <form action="/answer-delete" method="POST" style="display:inline;">
+                                                        <input type="hidden" name="id" value="{{ $answer->id }}">
+                                                        <button type="submit"
+                                                                onclick="return confirm('Are you sure you want to delete this answer?')"
+                                                                class="px-3 py-1 text-sm text-white bg-red-600 rounded-lg hover:bg-red-400 transition-colors transition-transform duration-100 ease-in-out transform hover:scale-105">
+                                                            <i class="fa-solid fa-trash"></i>
+                                                        </button>
+                                                    </form>
+                                                </div>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                    @else
+                        <p class="text-center">No answers yet.</p>
+                    @endif
+                </div>
+                <div class="flex justify-center">
+                    @if(isset($_SESSION['user_id']))
+                        <a href="/add-answer" class="inline-block text-black hover:underline focus:ring-1 font-medium rounded-lg text-sm px-5 py-2.5">
+                            <x-submit-button>Add answer</x-submit-button>
+                        </a>
+                    @else
+                        <p><em>You must be logged in to add an answer.</em></p>
+                    @endif
+                    @else
+                        <h1>Question not found</h1>
+                    @endif
+                </div>
+            </div>
         </div>
 @endsection
